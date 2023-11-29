267eaedf7dd2a56c5ac1ac953d5a185e80bb6bef
ilmanvillvay <ilman@villvay.com>
2023-11-29 20:25:05 +0530

    implement metrics, state, checkpoints

--- src/main/java/com/villvay/dataprocessor/mapper/DataProcessor.java
+++ src/main/java/com/villvay/dataprocessor/mapper/DataProcessor.java
@@ -1,15 +1,30 @@
 package com.villvay.dataprocessor.mapper;
 
+import com.codahale.metrics.SlidingWindowReservoir;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.networknt.schema.ValidationMessage;
 import com.villvay.dataprocessor.util.StreamUtils;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.dropwizard.metrics.DropwizardHistogramWrapper;
+import org.apache.flink.dropwizard.metrics.DropwizardMeterWrapper;
+import org.apache.flink.metrics.Counter;
+import org.apache.flink.metrics.Histogram;
+import org.apache.flink.metrics.Meter;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
 import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Iterator;
 import java.util.Set;
 
 
@@ -17,9 +32,19 @@ import java.util.Set;
  * @author Ilman Iqbal
  * 11/16/2023
  */
-public class DataProcessor<T> extends ProcessAllWindowFunction<String, T, TimeWindow> {
+public class DataProcessor<T> extends ProcessAllWindowFunction<String, T, TimeWindow> implements CheckpointedFunction {
 
     private static final Logger LOG = LoggerFactory.getLogger(DataProcessor.class);
+
+    private transient Counter counter;
+
+    private transient Histogram histogram;
+
+    private transient Meter meter;
+
+    private transient ListState<Long> processedRecordCount;
+
+    long oldCounterValue = 0L;
 
     Class<T> pojoClass;
     String jsonSchema;
@@ -31,11 +56,36 @@ public class DataProcessor<T> extends ProcessAllWindowFunction<String, T, TimeWi
         this.jsonFields = jsonFields;
     }
 
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+
+        // A Counter is used to count something.
+        this.counter = getRuntimeContext()
+                .getMetricGroup()
+                .counter("myCounter");
+
+        // A Histogram measures the distribution of long values. ex: timestamp
+        com.codahale.metrics.Histogram dropwizardHistogram =
+                new com.codahale.metrics.Histogram(new SlidingWindowReservoir(50000));
+        this.histogram = getRuntimeContext()
+                .getMetricGroup()
+                .histogram("myHistogram", new DropwizardHistogramWrapper(dropwizardHistogram));
+
+        // A Meter measures an average throughput.
+        com.codahale.metrics.Meter dropwizardMeter = new com.codahale.metrics.Meter();
+        this.meter = getRuntimeContext()
+                .getMetricGroup()
+                .meter("myMeter", new DropwizardMeterWrapper(dropwizardMeter));
+    }
 
     @Override
     public void process(ProcessAllWindowFunction<String, T, TimeWindow>.Context context, Iterable<String> elements, Collector<T> out) throws Exception {
         elements.forEach(value -> {
-            long start = System.currentTimeMillis();
+            this.counter.inc();
+            this.histogram.update(System.currentTimeMillis());
+            this.meter.markEvent();
+
             JsonNode jsonValue = StreamUtils.getJsonNodeByFiledName(value, jsonFields);
             Set<ValidationMessage> groupErrors = StreamUtils.validateJsonByJsonSchema(jsonValue, jsonSchema);
 
@@ -43,11 +93,38 @@ public class DataProcessor<T> extends ProcessAllWindowFunction<String, T, TimeWi
                 LOG.error("Errors while processing json {}", value);
                 LOG.error("Errors {}", groupErrors);
             } else {
-                T pojo = StreamUtils.mapJsonToPojo(jsonValue, pojoClass);
-                long time = System.currentTimeMillis() - start;
-                LOG.warn("============== time taken in millis for mapping {}", time);
-                out.collect(pojo);
+                out.collect(StreamUtils.mapJsonToPojo(jsonValue, pojoClass));
             }
         });
+    }
+
+    // Whenever a checkpoint has to be performed, snapshotState() is called.
+    @Override
+    public void snapshotState(FunctionSnapshotContext context) throws Exception {
+
+        if (oldCounterValue != counter.getCount()) {
+            Iterator<Long> processedRecordCountIterator = processedRecordCount.get().iterator();
+            Long oldProcessedRecordCount = 0L;
+            if (processedRecordCountIterator.hasNext()) {
+                oldProcessedRecordCount = processedRecordCountIterator.next();
+            }
+            long newProcessedRecordCount = oldProcessedRecordCount + counter.getCount();
+            processedRecordCount.clear();
+            processedRecordCount.add(newProcessedRecordCount);
+            this.oldCounterValue = this.counter.getCount();
+            LOG.info("processedRecordCount state value updated {}", newProcessedRecordCount);
+        }
+
+    }
+
+    //initializeState(), is called every time the user-defined function is initialized
+    @Override
+    public void initializeState(FunctionInitializationContext context) throws Exception {
+        ListStateDescriptor<Long> descriptor = new ListStateDescriptor<>(
+                "processed-record-count",
+                TypeInformation.of(new TypeHint<Long>() {
+                }));
+
+        processedRecordCount = context.getOperatorStateStore().getUnionListState(descriptor);
     }
 }
--- src/main/java/com/villvay/dataprocessor/processor/impl/ItemProcessor.java
+++ src/main/java/com/villvay/dataprocessor/processor/impl/ItemProcessor.java
@@ -12,10 +12,12 @@ import com.villvay.dataprocessor.source.StreamSourceFactory;
 import com.villvay.dataprocessor.source.impl.KafkaStreamSource;
 import com.villvay.dataprocessor.util.StreamUtils;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
@@ -24,6 +26,7 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Ilman Iqbal
@@ -44,6 +47,21 @@ public class ItemProcessor implements StreamProcessor {
 
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        env.enableCheckpointing(500); // Enable checkpointing with a 500ms interval
+        CheckpointConfig checkpointConfig = env.getCheckpointConfig(); // Configure checkpointing behavior
+        checkpointConfig.setMinPauseBetweenCheckpoints(500); // make sure 500 ms of progress happen between checkpoints
+        checkpointConfig.setCheckpointTimeout(60000); // checkpoints have to complete within one minute, or are discarded
+        checkpointConfig.setExternalizedCheckpointCleanup(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION); // Retain the checkpoint when the job is cancelled.
+        String projectDirectory = System.getProperty("user.dir");
+
+        checkpointConfig.setCheckpointStorage("file:" + projectDirectory + "/checkpoints");
+
+        // If checkpointing is enabled, the default value is fixed-delay with Integer.MAX_VALUE restart attempts and '1 s' delay.
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(
+                3, // Number of restart attempts
+                org.apache.flink.api.common.time.Time.of(10, TimeUnit.SECONDS) // Delay between restarts
+        ));
+
         KafkaStreamSource kafkaStreamSource = ((KafkaStreamSource) StreamSourceFactory.getInstance(ConnectorType.KAFKA));
         ActiveMqStreamSink activeMqStreamSink = (ActiveMqStreamSink) StreamSinkFactory.getInstance(ConnectorType.ACTIVE_MQ);
 
@@ -56,22 +74,29 @@ public class ItemProcessor implements StreamProcessor {
         DataProcessor<ItemDto> itemDataProcessor = new DataProcessor<>(ItemDto.class,
                 itemParameters.get("sinks.activemq.json_schema"), "payload");
 
-        SingleOutputStreamOperator<ItemDto> itemDtoStream = kafkaSource
+        SingleOutputStreamOperator<ItemDto> itemDtoStream = kafkaSource.uid("item-kafka-soruce")
+                .setParallelism(4)
                 .windowAll(TumblingProcessingTimeWindows.of(Time.seconds(10)))
                 .allowedLateness(Time.seconds(2))
-                .process(itemDataProcessor, TypeInformation.of(ItemDto.class))
+                .process(itemDataProcessor, TypeInformation.of(ItemDto.class)).uid("item-process")
                 .returns(ItemDto.class);
 
         SingleOutputStreamOperator<String> itemDtoJsonStream = itemDtoStream
-                .filter(Objects::nonNull)
-                .map(value -> new ObjectMapper().writeValueAsString(value))
+                .filter(Objects::nonNull).uid("item-filter")
+                .setParallelism(4)
+                .map(value -> new ObjectMapper().writeValueAsString(value)).uid("item-map")
+                .setParallelism(4)
                 .returns(String.class);
 
-        itemDtoJsonStream.addSink(activeMqStreamSink.getSink(this.itemParameters));
+        itemDtoJsonStream.addSink(activeMqStreamSink.getSink(this.itemParameters)).uid("item-activemq-sink")
+                .setParallelism(4);
 
         LOG.info("Successfully assigned kafka sink connector for item processor");
 
         try {
+            env.setMaxParallelism(4);
+            env.disableOperatorChaining(); // if this is not disables, then flink job graph will show all operators in one thread if there is no shuffling/mixing involved.
+
             env.execute("Item Processor Job");
         } catch (Exception e) {
             throw new DataProcessorException("Error when executing item processor job", e);
